85a2daa1a8c77b1b8b9959aae14dd29297d83c94
Jinguang Liu <jinguang.liu@qq.com>
2017-10-30 05:45:58 -0400

    add MACD investigation

--- machine-learning-pro/matplotlib_study/finance/test_candlestick.py
+++ machine-learning-pro/matplotlib_study/finance/test_candlestick.py
@@ -16,21 +16,22 @@ import matplotlib.pyplot as plt
 import matplotlib.finance as mpf
 
 if __name__ == '__main__':
-    data = ts.get_k_data('399300', index=True, start='2017-01-01')
-    sma_10 = talib.SMA(np.array(data['close']), 10)
-    sma_30 = talib.SMA(np.array(data['close']), 30)
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_xticks(range(0, len(data['date']), 10))
-    
-    ax.set_xticklabels(data['date'][::10], rotation=45)
-    ax.plot(sma_10, label='10')
-    ax.plot(sma_30, label='30')
-    ax.legend(loc='upper left')
-    
-    mpf.candlestick2_ochl(ax, data['open'], data['close'], data['high'], data['low'],
-                     width=0.5, colorup='r', colordown='green',
-                     alpha=0.6)
-    plt.grid()    
-    plt.show()
+    data = ts.get_k_data('399300', index=True, start='2017-10-01')
+    print data
+#     sma_10 = talib.SMA(np.array(data['close']), 10)
+#     sma_30 = talib.SMA(np.array(data['close']), 30)
+#     
+#     fig = plt.figure()
+#     ax = fig.add_subplot(1, 1, 1)
+#     ax.set_xticks(range(0, len(data['date']), 10))
+#     
+#     ax.set_xticklabels(data['date'][::10], rotation=45)
+#     ax.plot(sma_10, label='10')
+#     ax.plot(sma_30, label='30')
+#     ax.legend(loc='upper left')
+#     
+#     mpf.candlestick2_ochl(ax, data['open'], data['close'], data['high'], data['low'],
+#                      width=0.5, colorup='r', colordown='green',
+#                      alpha=0.6)
+#     plt.grid()    
+#     plt.show()
